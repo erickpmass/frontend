2278ecaca3469e8cc04e451f5961cdd434bcac1b
peddrovieira <pe.bonangelo@gmail.com>
2023-04-06 21:56:05 -0300

    Angular-com-rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
+import { Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { CadastrarProdutosComponent } from './components/produtos/cadastrar-produtos/cadastrar-produtos.component';
+import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListarProdutosComponent,
+    CadastrarProdutosComponent
   ],
   imports: [
     BrowserModule,
