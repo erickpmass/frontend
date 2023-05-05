848b4cf8bfe2085ec1237a8a1395647fd8cb4c7d
erickpmass <erickpereiramassuco@gmail.com>
2023-05-04 22:35:35 -0300

    Listar Produtos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  logo: string = '../assets/Img/logo_horacio.png';
+
+  nomeProduto: string = 'Curso de Angular';
+  anuncio: string = `O ${this.nomeProduto} está em promoção`;
+  idProduto: number = 3.5123;
+  precoProduto: number = 2.51;
+  promocao: boolean = true;
+  foto: string = 'assets/img/crud.png';
+    constructor() {
+
+    console.log('Nome do Produto', this.nomeProduto);
+    }
 }
